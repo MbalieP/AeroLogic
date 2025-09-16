4222f757df8c521ca3262a192a84604cf5266779
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-16 18:45:26 +0200

    flights

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Flight(models.model):
-    origin = models.charField(max_length=64)
-    destination = models.charField(max_lenght=64)
-    duration = models.integerField()
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
