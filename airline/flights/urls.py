26d04feef93065eec9409cfd2f468bf86af8f3d2
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-17 11:47:20 +0200

    passengers

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    Path("<int:flight_id>", views.flight, name="flight")
+    path("<int:flight_id>", views.flight, name="flight")
 ]
