d41f78a0777645e16973f963c34527a5bb085525
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-17 08:16:08 +0200

    fixed display

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -6,7 +6,7 @@ class Airport(models.Model):
     city = models.CharField(max_length=64)
 
     def __str__(self):
-        return f"{self.city}({self.code})"
+        return f"{self.city} ({self.code})"
 
 
 
@@ -18,4 +18,6 @@ class Flight(models.Model):
 
 
     def __str__(self):
-        return f"{self.id}:{self.origin} to {self.destination}"
+        return f"{self.id}: {self.origin} to {self.destination}"
+    
+    
